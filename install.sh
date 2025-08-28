3e71339fdb1a54799085cb78a2b2ac1fd2267f96
zevaryx <zevaryx@gmail.com>
2025-08-28 19:34:58 +0000

    Update installer to be architecture independent

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 set -e
 
+ARCH=$(uname -m)
+
 echo "Installing into ~/.local/bin..."
 mkdir -p ~/.local/bin
 
 cd ~/.local/bin
-wget "https://git.zevaryx.com/zevaryx/hasher-rs/releases/download/latest/hasher-gnu-linux-x86_64" -O ~/.local/bin/hasher-gnu-linux-x86_64
-chmod +x ~/.local/bin/hasher-gnu-linux-x86_64
-wget "https://git.zevaryx.com/zevaryx/hasher-rs/releases/download/latest/hasher-gnu-linux-x86_64.sha256" -O ~/.local/bin/hasher-gnu-linux-x86_64.sha256
-sha256sum --status -c hasher-gnu-linux-x86_64.sha256 || exit 1
-mv hasher-gnu-linux-x86_64 hasher
-rm hasher-gnu-linux-x86_64.sha256
+wget "https://git.zevaryx.com/zevaryx/hasher-rs/releases/download/latest/hasher-linux-$ARCH" -O ~/.local/bin/hasher-linux-$ARCH
+chmod +x ~/.local/bin/hasher-linux-$ARCH
+wget "https://git.zevaryx.com/zevaryx/hasher-rs/releases/download/latest/hasher-linux-$ARCH.sha256" -O ~/.local/bin/hasher-linux-$ARCH.sha256
+sha256sum --status -c hasher-linux-$ARCH.sha256 || exit 1
+mv hasher-linux-$ARCH hasher
+rm hasher-linux-$ARCH.sha256
 
 echo "Done! Add ~/.local/bin to your path to use!"
